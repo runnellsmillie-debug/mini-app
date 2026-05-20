c4d7f25ca4d715b9d2b7e682059a88da6d15213a
runnellsmillie-debug <runnellsmillie@gmail.com>
2026-05-20 23:15:41 +0500

    Update bat.py

--- bat.py
+++ bat.py
@@ -9,7 +9,7 @@ from flask import Flask
 from threading import Thread
 
 # 1. Tokenni Environment Variable'dan o'qiymiz
-TOKEN = os.getenv("8619700756:AAHFrcQYILDeM8_ELs5sFNguL9WfDZhL3VQ") 
+TOKEN = os.getenv("BOT_TOKEN") 
 WEB_APP_URL = "https://runnellsmillie-debug.github.io/mini-app/"
 
 # Logging'ni sozlash
